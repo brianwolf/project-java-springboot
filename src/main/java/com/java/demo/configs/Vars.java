9955411bf4abc41a7c150ca028248e36c985d6bf
brianlobo <blobo@redhat.com>
2023-01-16 11:10:20 -0300

    Se saca el config vars porque ya no se usa

--- src/main/java/com/java/demo/configs/Vars.java
+++
@@ -1,5 +0,0 @@
-package com.java.demo.configs;
-
-public enum Vars {
-    VERSION
-}
--- src/main/java/com/java/demo/controllers/AdminController.java
+++ src/main/java/com/java/demo/controllers/AdminController.java
@@ -5,15 +5,13 @@ import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-
-import com.java.demo.configs.ConfigVars;
-import com.java.demo.configs.Vars;
 
 @RestController
 @RequestMapping("/")
@@ -22,29 +20,17 @@ public class AdminController {
     private static Logger logger = LoggerFactory.getLogger(AdminController.class);
 
     @Autowired
-    private ConfigVars configVars;
+    private Environment env;
 
     @GetMapping("/")
     public ResponseEntity<Object> getMessages() {
 
-        String version = configVars.get(Vars.VERSION);
+        String version = env.getProperty("VERSION");
 
         HashMap<String, Object> result = new HashMap<>();
         result.put("version", version);
 
         logger.info(String.format("version = %s", version));
-
-        return ResponseEntity.status(200).body(result);
-    }
-
-    @GetMapping("/vars")
-    public ResponseEntity<Object> getVars() {
-
-        HashMap<String, Object> result = new HashMap<>();
-
-        for (Vars var : Vars.values()) {
-            result.put(var.toString(), configVars.get(var));
-        }
 
         return ResponseEntity.status(200).body(result);
     }
